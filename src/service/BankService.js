abb8a46f4587b878c6bb1349e37c544b13f0f39a
demir45 <demir45u@yahoo.com>
2022-02-06 00:04:47 +0300

    v_04

--- src/service/BankService.js
+++ src/service/BankService.js
@@ -1,6 +1,7 @@
 import axiosInstance from './axiosInstance';
 
-const BASE_URL = 'http://localhost:8090';
+// const BASE_URL = 'http://localhost:8090';
+const BASE_URL = 'https://bankofprl.herokuapp.com/';
 
 class BankService {
 	login(userInfo) {
